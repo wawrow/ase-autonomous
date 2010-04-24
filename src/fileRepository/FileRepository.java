b46864f848577a964cc4f8e42f1ec7484c185f35
fergus <fergus@localhost>
2010-04-24 23:24:05 +0100

    Initial commit of test use of JGroups

---
+++ src/fileRepository/FileRepository.java
@@ -0,0 +1,11 @@
+package fileRepository;
+
+public class FileRepository {
+	
+	public static void main(String[] args) {
+		try {
+		    new Node().start();
+		} catch (Exception e){			
+		}
+	}
+}
---
+++ src/fileRepository/Node.java
@@ -0,0 +1,46 @@
+package fileRepository;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import org.jgroups.JChannel;
+import org.jgroups.Message;
+import org.jgroups.ReceiverAdapter;
+import org.jgroups.View;
+
+public class Node extends ReceiverAdapter {
+    JChannel channel;
+  
+    public void start() throws Exception {
+        channel=new JChannel();
+        channel.setReceiver(this);
+        channel.connect("FileRepositoryCluster");
+        eventLoop();
+        channel.close();
+    }
+    
+    private void eventLoop() {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        while(true) {
+        	try {
+                System.out.print("> "); System.out.flush();
+                String line=in.readLine().toLowerCase();
+                if(line.startsWith("quit") || line.startsWith("exit")) {
+                    break;
+                }
+                Message msg=new Message(null, null, line);
+                channel.send(msg);
+            } catch(Exception e) {
+                System.out.println("Exception encountered e=" + e.toString());	
+            }
+        }
+    }
+    
+    public void viewAccepted(View new_view) {
+        System.out.println("** view: " + new_view);
+    }
+
+    public void receive(Message msg) {
+        System.out.println(msg.getSrc() + ": " + msg.getObject());
+    }
+}
+     
