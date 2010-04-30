22dd642807e39fd08285107e423a9c7a9afcab74
Keith Brady <keith@slard.com>
2010-04-30 21:50:00 +0100

    Commit Fergus's changes with one small tweak.

--- src/com/slard/filerepository/DataStore.java
+++ src/com/slard/filerepository/DataStore.java
@@ -1,24 +1,26 @@
 package com.slard.filerepository;
 
+import java.io.IOException;
 import java.util.Vector;
 
-//Provides internal node interface into Data Store operations
+// Provides internal node interface into Data Store operations
 public interface DataStore {
+
+	// Fetches all Objects
+	public Vector<DataObject> getAllDataObjects();
+
+	// Gets object by name from storage
+	public abstract DataObject getDataObject(String name) throws IOException;
+
+	// Removes object from DataStore 
+	public abstract void deleteDataObject(String name) throws IOException;
 	
-	//Gets object by name from storage
-	public abstract DataObject GetDataObject(String name);
-	
-	//Returns whether particular object is contained in storage
-	public abstract Boolean Contains(String name);
-	
-	//Removes object from DataStore 
-	public abstract void Delete(String name);
-	
-	//Stores object in DataStore
-	public abstract void StoreDataObject(DataObject dataObject);
-	
-	//Fetches all Objects
-	public Vector<DataObject> GetAllDataObjects();
-	
-	//We'll probably need also methods for free disk space usage etc. we'll add them as required.
+	// Stores object in DataStore
+	public abstract void storeDataObject(DataObject dataObject) throws IOException;
+
+	// Replaces object in DataStore
+	public abstract void replaceDataObject(DataObject dataObject) throws Exception;
+
+	// Returns whether particular object is contained in storage
+	public abstract Boolean contains(String name);		
 }
