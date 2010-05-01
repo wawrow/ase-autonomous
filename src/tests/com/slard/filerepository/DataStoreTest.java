37e72c1f0c7aa3ddb3906db26ef8b28031ef5add
Wawrzyniec <Wawrzyniec@localhost>
2010-05-01 14:00:28 +0100

    added mockito

--- src/tests/com/slard/filerepository/DataStoreTest.java
+++ src/tests/com/slard/filerepository/DataStoreTest.java
@@ -8,9 +8,9 @@ import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import com.slard.filerepository.DataObject;
-import com.slard.filerepository.DataObjectImpl;
 import com.slard.filerepository.DataStore;
 import com.slard.filerepository.DataStoreImpl;
 
@@ -19,12 +19,17 @@ public class DataStoreTest {
   String TESTFILENAME = "testName";
   byte[] TESTDATA  = new byte[] { 0, 1, 1 };  
   DataStore dataStore;
+  DataObject mockedDataObject;
   
   @Before
   public void setUp() throws Exception {
     dataStore = new DataStoreImpl();
-    DataObject dataObject= new DataObjectImpl(TESTFILENAME, TESTDATA);
-    this.dataStore.storeDataObject(dataObject);
+    
+    //Set Up Mocks
+    mockedDataObject = Mockito.mock(DataObject.class);
+    Mockito.when(mockedDataObject.getData()).thenReturn(TESTDATA);
+    Mockito.when(mockedDataObject.getName()).thenReturn(TESTFILENAME);
+    this.dataStore.storeDataObject(mockedDataObject);    
   }
 
   @Test
@@ -43,10 +48,14 @@ public class DataStoreTest {
     Assert.assertTrue(Arrays.equals(dao.getData(), TESTDATA));
   }
 
+  @Test
+  public void VerifyMocks() throws Exception{
+    Mockito.verify(mockedDataObject, Mockito.atLeast(1)).getName();
+    Mockito.verify(mockedDataObject, Mockito.atLeast(1)).getData();
+  }
   
   @After
   public void tearDown() throws Exception {
-    
   }
 
 }
