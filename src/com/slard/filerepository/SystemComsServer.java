81f813d883010319f6957022cb3d0c72e4915169
Wawrzyniec <Wawrzyniec@localhost>
2010-05-01 13:31:20 +0100

    added example - working - test for Data Store impl, no mocking framework for time being

--- src/com/slard/filerepository/SystemComsServer.java
+++ src/com/slard/filerepository/SystemComsServer.java
@@ -5,48 +5,58 @@ import org.jgroups.MembershipListener;
 import org.jgroups.MessageListener;
 import org.jgroups.blocks.RpcDispatcher;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Vector;
 
 /**
- * Created by IntelliJ IDEA.
- * User: kbrady
- * Date: 28-Apr-2010
- * Time: 01:24:59
- * To change this template use File | Settings | File Templates.
+ * Created by IntelliJ IDEA. User: kbrady Date: 28-Apr-2010 Time: 01:24:59 To
+ * change this template use File | Settings | File Templates.
  */
 public class SystemComsServer implements SystemComs {
 
-  private DataStore store = null;
-  private RpcDispatcher dispatcher = null;
+	private DataStore store = null;
+	private RpcDispatcher dispatcher = null;
 
-  public SystemComsServer(Channel channel, DataStore store, MessageListener messages, MembershipListener members) {
-    this.store = store;
-    this.dispatcher = new RpcDispatcher(channel, messages, members, this);
-  }
+	public SystemComsServer(Channel channel, DataStore store,
+			MessageListener messages, MembershipListener members) {
+		this.store = store;
+		this.dispatcher = new RpcDispatcher(channel, messages, members, this);
+	}
 
-  @Override
-  public Map<Long, NodeDescriptor> getNodelist() {
-    return null;  //To change body of implemented methods use File | Settings | File Templates.
-  }
+	@Override
+	public Map<Long, NodeDescriptor> getNodelist() {
+		return null; // To change body of implemented methods use File |
+		// Settings | File Templates.
+	}
 
-  @Override
-  public Vector<String> syncFilelist() {
-    return null;  //To change body of implemented methods use File | Settings | File Templates.
-  }
+	@Override
+	public Vector<String> syncFilelist() {
+		return null; // To change body of implemented methods use File |
+		// Settings | File Templates.
+	}
 
-  @Override
-  public Boolean store(DataObject dataObject) {
-    store.StoreDataObject(dataObject);
-    return true;
-  }
+	@Override
+	public Boolean store(DataObject dataObject) {
+		try {
+			store.storeDataObject(dataObject);
+		} catch (IOException ex) {
+			return false;
+		}
+		return true;
+	}
 
-  @Override
-  public DataObject retrieve(String name) {
-    return store.GetDataObject(name);
-  }
+	@Override
+	public DataObject retrieve(String name) {
+	  try{
+		return store.getDataObject(name);
+	  } catch(IOException ex){
+	    return null;
+	  }
+	  
+	}
 
-  public void stop() {
-    dispatcher.stop();
-  }
+	public void stop() {
+		dispatcher.stop();
+	}
 }
