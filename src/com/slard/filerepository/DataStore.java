c7021bf501892b76d4032d77dd16093f03069507
Wawrzyniec <Wawrzyniec@localhost>
2010-05-03 03:24:13 +0100

    Done Filelist high level stuff - no low level

--- src/com/slard/filerepository/DataStore.java
+++ src/com/slard/filerepository/DataStore.java
@@ -3,7 +3,7 @@ package com.slard.filerepository;
 import java.util.ArrayList;
 
 // Provides internal node interface into Data Store operations
-public interface DataStore extends FileOperations {
+public interface DataStore extends FileOperations, SystemFileList {
 
   // Gets the object store location
   String getStoreLocation();
