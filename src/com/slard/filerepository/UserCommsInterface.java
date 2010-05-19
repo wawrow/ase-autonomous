1417dcd538ea0fb3d44208111dbf468149e34b54
Keith Brady <keith@slard.com>
2010-05-19 08:47:04 +0100

    Small cleanups and docs

--- src/com/slard/filerepository/UserCommsInterface.java
+++ src/com/slard/filerepository/UserCommsInterface.java
@@ -9,28 +9,71 @@ import java.util.List;
  * User: kbrady
  * Date: 15-May-2010
  * Time: 17:52:18
- * To change this template use File | Settings | File Templates.
+ * Interface presented by servers to clients.
  */
 public interface UserCommsInterface {
+  /**
+   * Default client cluster name.
+   */
   static final String CHANNEL_NAME = "ClientCluster";
+  /**
+   * Default client listen port.
+   */
   static final String CLIENT_PORT = "45589";
+  /**
+   * Allows us to override the channel name.
+   */
   static final String CLIENT_COMMS_PROP = "client.channel.name";
+  /**
+   * The property we need to change to affect the listen port.
+   */
   static final String JGROUPS_PORT_PROP = "jgroups.udp.mcast_port";
 
+  /**
+   * Are we a server?
+   *
+   * @return true for anything that fully implements this interface (not stubs).
+   */
   Boolean isServer();
 
-  // Clients may ask about whether this node is the master of a file
-
+  /**
+   * Clients may ask about whether this node is the master of a file.
+   *
+   * @param name Filename to check.
+   * @return true if we are the master for this file.
+   */
   Address isMaster(String name);
 
-  // Clients may ask about file ownership before directing their requests
-
+  /**
+   * Do we have a file?
+   *
+   * @param name The file to check.
+   * @return Address of master of the file.
+   */
   Address hasFile(String name);
 
+  /**
+   * List all files we have.
+   *
+   * @param regex Pattern to select which files to report.
+   * @return Filtered list of files.
+   */
   List<String> getFileNames(String regex);
 
+  /**
+   * Store a file. Uses eventual consistency.
+   *
+   * @param file The file.
+   * @return true if master has file.
+   */
   Boolean store(DataObject file);
 
+  /**
+   * Store a file. Uses guaranteed consistency.
+   *
+   * @param file The file.
+   * @return True if master and all replicas have the file.
+   */
   Boolean storeAll(DataObject file);
 
   DataObject retrieve(String name);
